b53ea98e01a83669b92ddb4ef6c720c4e0c7cb6c
Genrevvv <genrevvaguilar@gmail.com>
2025-05-24 16:39:44 +0800

    Update README.md and add the schema.sql

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    date TEXT NOT NULL,
+    caption TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    friend_id INT NOT NULL,
+    status ENUM('P', 'F') NOT NULL DEFAULT 'P',
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (friend_id) REFERENCES users(id),
+    UNIQUE KEY unique_friendship (user_id, friend_id)
+);
